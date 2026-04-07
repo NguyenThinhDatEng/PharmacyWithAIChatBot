45e490409a7bfd55675016ebe67fa169319b40f2
NguyenThinhDatEng <thinh.nv194178@sis.hust.edu.vn>
2026-04-07 22:29:59 +0700

    build

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/PharmacyWithAIChatBot/'),
+  history: createWebHistory('/'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
